ffc7830402e517e60465b499b961b81f7a6c4f7b
Yokior <844599449@qq.com>
2024-03-20 14:08:13 +0800

    [init]: 添加gateway网关 使用nacos管理

---
+++ rice-gateway/src/main/java/com/rice/ricegateway/RiceGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.rice.ricegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiceGatewayApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(RiceGatewayApplication.class, args);
+    }
+
+}
