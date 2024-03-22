00cce757cbfb0cb3539c17459326074cd61fb2c7
Yokior <844599449@qq.com>
2024-03-22 19:09:50 +0800

    [init]: 前后端简单联调 解决跨域问题 [bugfix]: 修复数据库nacos共享配置问题 改为单独配置

--- rice-product/src/main/java/com/rice/product/controller/CategoryController.java
+++ rice-product/src/main/java/com/rice/product/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package com.rice.product.controller;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +17,6 @@ import com.rice.common.utils.PageUtils;
 import com.rice.common.utils.R;
 
 
-
 /**
  * 商品三级分类
  *
@@ -26,18 +26,20 @@ import com.rice.common.utils.R;
  */
 @RestController
 @RequestMapping("product/category")
-public class CategoryController {
+public class CategoryController
+{
     @Autowired
     private CategoryService categoryService;
 
     /**
-     * 列表
+     * 查出所有分类以及子分类，以树形结构组装起来
      */
-    @RequestMapping("/list")
-    public R list(@RequestParam Map<String, Object> params){
-        PageUtils page = categoryService.queryPage(params);
+    @RequestMapping("/list/tree")
+    public R list()
+    {
+        List<CategoryEntity> categoryEntityList = categoryService.listWithTree();
 
-        return R.ok().put("page", page);
+        return R.ok().put("data", categoryEntityList);
     }
 
 
@@ -45,8 +47,9 @@ public class CategoryController {
      * 信息
      */
     @RequestMapping("/info/{catId}")
-    public R info(@PathVariable("catId") Long catId){
-		CategoryEntity category = categoryService.getById(catId);
+    public R info(@PathVariable("catId") Long catId)
+    {
+        CategoryEntity category = categoryService.getById(catId);
 
         return R.ok().put("category", category);
     }
@@ -55,8 +58,9 @@ public class CategoryController {
      * 保存
      */
     @RequestMapping("/save")
-    public R save(@RequestBody CategoryEntity category){
-		categoryService.save(category);
+    public R save(@RequestBody CategoryEntity category)
+    {
+        categoryService.save(category);
 
         return R.ok();
     }
@@ -65,8 +69,9 @@ public class CategoryController {
      * 修改
      */
     @RequestMapping("/update")
-    public R update(@RequestBody CategoryEntity category){
-		categoryService.updateById(category);
+    public R update(@RequestBody CategoryEntity category)
+    {
+        categoryService.updateById(category);
 
         return R.ok();
     }
@@ -75,8 +80,9 @@ public class CategoryController {
      * 删除
      */
     @RequestMapping("/delete")
-    public R delete(@RequestBody Long[] catIds){
-		categoryService.removeByIds(Arrays.asList(catIds));
+    public R delete(@RequestBody Long[] catIds)
+    {
+        categoryService.removeByIds(Arrays.asList(catIds));
 
         return R.ok();
     }
--- rice-product/src/main/java/com/rice/product/service/impl/CategoryServiceImpl.java
+++ rice-product/src/main/java/com/rice/product/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,13 @@
 package com.rice.product.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -14,10 +20,15 @@ import com.rice.product.service.CategoryService;
 
 
 @Service("categoryService")
-public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity> implements CategoryService {
+public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity> implements CategoryService
+{
+
+    @Autowired
+    private CategoryDao categoryDao;
 
     @Override
-    public PageUtils queryPage(Map<String, Object> params) {
+    public PageUtils queryPage(Map<String, Object> params)
+    {
         IPage<CategoryEntity> page = this.page(
                 new Query<CategoryEntity>().getPage(params),
                 new QueryWrapper<CategoryEntity>()
@@ -25,5 +36,45 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
 
         return new PageUtils(page);
     }
+
+    @Override
+    public List<CategoryEntity> listWithTree()
+    {
+        // 查出所有分类
+        List<CategoryEntity> categoryList = categoryDao.selectList(null);
+
+
+        // 组装成父子树形结构
+
+        // 查询所有的一级分类
+        List<CategoryEntity> lv1Category = categoryList.stream()
+                .filter(c -> c.getParentCid() == 0)
+                .peek(categoryEntity -> categoryEntity.setChildren(getChildren(categoryEntity, categoryList)))
+                .sorted((m1, m2) -> {
+                    return (m1.getSort() == null ? 0 : m1.getSort()) - (m2.getSort() == null ? 0 : m2.getSort());
+                })
+                .collect(Collectors.toList());
+
+        return lv1Category;
+    }
+
+
+    private List<CategoryEntity> getChildren(CategoryEntity categoryEntity, List<CategoryEntity> categoryList)
+    {
+        Long catId = categoryEntity.getCatId();
+
+        List<CategoryEntity> childrenList = categoryList.stream()
+                // 如果filter后没有数据 自动return 所以不用加结束条件
+                .filter(c -> catId.equals(c.getParentCid()))
+                // 处理子分类的子分类
+                .peek(category -> category.setChildren(getChildren(category, categoryList)))
+                .sorted((m1, m2) -> {
+                    return (m1.getSort() == null ? 0 : m1.getSort()) - (m2.getSort() == null ? 0 : m2.getSort());
+                })
+                .collect(Collectors.toList());
+
+        return childrenList;
+    }
+
 
 }
